b75b0599ac7d54aacb636d05bf7971a3958e393b
sadeesha <sadeeshamanujaya99@gmail.com>
2023-06-15 10:25:01 +0530

    rev1.0

---
+++ main.js
@@ -0,0 +1,21 @@
+var input = document.getElementById("input");
+var output = document.getElementById("output");
+
+// This function clear all the values
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
+
+// This function display values
+function display(value) {
+  document.getElementById("result").value += value;
+}
+
+// This function evaluates the expression and returns result
+function calculate() {
+  var p = document.getElementById("result").value;
+  var q = eval(p);
+  document.getElementById("result").value = q;
+}
+
+input.addEventListener("keyup", calculate);
